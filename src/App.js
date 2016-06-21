6484362f7fcb4c7e08e6f92b88615cc8ef91e7a8
Andreas Moor <github@grmnd.ch>
2016-06-21 21:51:05 +0200

    App component in separate file

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { PropTypes, Component } from 'react'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+  render() {
+    return <h1>Hello, World!</h1>
+  }
+}
+
+export default App
